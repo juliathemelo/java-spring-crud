0ba35948929e5a1cbb4f25f12f410d64d66159d8
juliathemelo <juliarexsteam@hotmail.com>
2022-05-27 16:34:11 -0300

    start project

---
+++ src/main/java/com/first/project/javafirstprojet/JavinhaProjetoApplication.java
@@ -0,0 +1,12 @@
+package com.first.project.javafirstprojet;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class JavinhaProjetoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavinhaProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/project/javafirstprojet/JavinhaProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.project.javafirstprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavinhaProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
