25dbf63593aa31cb2d75b0ec07185e4ac116b6a0
juliathemelo <juliarexsteam@hotmail.com>
2022-06-01 13:27:20 -0300

    simple get and post

---
+++ src/main/java/com/first/project/javafirstprojet/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.first.project.javafirstprojet.repository;
+
+import com.first.project.javafirstprojet.model.UsuarioModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer> {
+
+}
